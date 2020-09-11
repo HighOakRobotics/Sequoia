818645fd0e364dd1a4cb7506c400b1c96dff475d
Brian Lu <lu_haoqin@outlook.com>
2020-09-11 14:14:08 -0500

    attempt unit

---
+++ test/com/ftc11392/sequoia/tests/SchedulerTest.java
@@ -0,0 +1,14 @@
+package com.ftc11392.sequoia.tests;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class SchedulerTest {
+
+	@Test
+	public void TestTwoPlusTwo() {
+		assertEquals(5, Integer.sum(2,2));
+	}
+}
