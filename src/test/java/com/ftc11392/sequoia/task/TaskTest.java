01b869b4d62ff1e64965a2636b8d526e9f624cf0
Brian Lu <lu_haoqin@outlook.com>
2020-09-11 19:21:06 -0500

    attempt to write Task Lifecycle test

---
+++ src/test/java/com/ftc11392/sequoia/task/TaskTest.java
@@ -0,0 +1,53 @@
+package com.ftc11392.sequoia.task;
+
+import com.ftc11392.sequoia.subsystem.Subsystem;
+import com.ftc11392.sequoia.util.OpModeState;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+public class TaskTest {
+
+	@DisplayName("Test Task Lifecycle")
+	@Test
+	public void TestTaskLifecycle() {
+		Scheduler.getInstance().cancelAll();
+		Scheduler.getInstance().clearSubsystems();
+		Scheduler.getInstance().clearBehaviors();
+		Scheduler.getInstance().init(mock(Telemetry.class));
+
+		Subsystem mockedSubsystem = mock(Subsystem.class);
+
+		Scheduler.getInstance().initSubsystems(mock(HardwareMap.class));
+
+		Scheduler.getInstance().loop(OpModeState.INIT_LOOP);
+
+		Task testTask = new Task() {
+			@Override
+			public void init() { }
+
+			@Override
+			public void loop() { }
+
+			@Override
+			public void stop(boolean interrupted) { }
+		};
+
+		Scheduler.getInstance().schedule(testTask);
+
+		Scheduler.getInstance().loop(OpModeState.RUN_LOOP);
+
+		assertTrue(testTask.isRunning());
+
+		Scheduler.getInstance().cancelAll();
+
+		Scheduler.getInstance().loop(OpModeState.RUN_LOOP);
+
+		assertFalse(testTask.isRunning());
+	}
+}
