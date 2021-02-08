321b3ac6cacb0c75f890e81c36f08f08549ed177
workstation <mail@example.com>
2021-02-07 18:31:48 -0600

    rollback init triggers

--- src/main/java/com/ftc11392/sequoia/SequoiaOpMode.java
+++ src/main/java/com/ftc11392/sequoia/SequoiaOpMode.java
@@ -12,30 +12,13 @@ public abstract class SequoiaOpMode extends OpMode {
 	protected GamepadHandler gamepad2H;
 	protected Scheduler scheduler;
 
-	private boolean initFlag = false;
-	private boolean initLoopFlag = false;
-	private boolean startFlag = false;
-	private boolean runLoopFlag = false;
-	private boolean stopFlag = false;
-
-	protected Trigger initTrigger = new Trigger(() -> initFlag);
-	protected Trigger initLoopTrigger = new Trigger(() -> initLoopFlag);
-	protected Trigger startTrigger = new Trigger(() -> startFlag);
-	protected Trigger runLoopTrigger = new Trigger(() -> runLoopFlag);
-	protected Trigger stopTrigger = new Trigger(() -> stopFlag);
-
 	@Override
 	public void init() {
-		resetFlags();
 		scheduler = Scheduler.getInstance();
 		resetScheduler();
 		scheduler.init(telemetry);
 		scheduler.initSubsystems(hardwareMap);
 		initTriggers();
-		initFlag = true;
-		scheduler.loop(OpModeState.INIT);
-		initFlag = false;
-		initLoopFlag = true;
 	}
 
 	@Override
@@ -45,17 +28,11 @@ public abstract class SequoiaOpMode extends OpMode {
 
 	@Override
 	public void start() {
-		initLoopFlag = false;
 		gamepad1H = new GamepadHandler(gamepad1);
 		gamepad2H = new GamepadHandler(gamepad2);
 		scheduler.clearBehaviors();
 		scheduler.startSubsystems();
 		runTriggers();
-		startFlag = true;
-		scheduler.loop(OpModeState.STARTED);
-		startFlag = false;
-		System.out.println("started opmode");
-		runLoopFlag = true;
 	}
 
 	@Override
@@ -65,11 +42,7 @@ public abstract class SequoiaOpMode extends OpMode {
 
 	@Override
 	public void stop() {
-		runLoopFlag = false;
-		stopFlag = true;
 		scheduler.stopSubsystems();
-		for (int i = 0; i < 2; i++)
-			scheduler.loop(OpModeState.STOPPED);
 		resetScheduler();
 	}
 
@@ -77,14 +50,6 @@ public abstract class SequoiaOpMode extends OpMode {
 		scheduler.cancelAll();
 		scheduler.clearBehaviors();
 		scheduler.clearSubsystems();
-	}
-
-	private void resetFlags() {
-		initFlag = false;
-		initLoopFlag = false;
-		startFlag = false;
-		runLoopFlag = false;
-		stopFlag = false;
 	}
 
 	/**
